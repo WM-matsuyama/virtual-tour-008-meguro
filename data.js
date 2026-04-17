3db2ef033fbc3eb066e0013485be4beeb7e2a131
WM-matsuyama <matsuyama@wallmate.co.jp>
2026-04-17 12:59:47 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-__vr_b",
+      "name": "目黒三田パークマンション_玄関_VR_B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.85582322503069,
+          "pitch": 0.1602996817199287,
+          "rotation": 0,
+          "target": "1-_ldk_vr_b_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-_ldk_vr_b_",
+      "name": "目黒三田パークマンション_LDK_VR_B_カーペット",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7109311459925145,
+          "pitch": 0.08386382751809407,
+          "rotation": 6.283185307179586,
+          "target": "0-__vr_b"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
